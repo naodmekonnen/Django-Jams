30cc7e2d780a02651b2b527284e5150989a80ab1
naodmekonnen <naod.mekonnen@student.awesomeinc.org>
2022-11-16 15:41:09 +0000

    created models

--- project_jam/myapp/models.py
+++ project_jam/myapp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Artist(models.Model):
+    name = models.CharField(max_length=225)
+    def __str__ (self):
+        return self.name
+
+class Album(models.Model):
+    title = models.CharField(max_length=225)
+    def __str__ (self):
+        return self.title
+    
+class Genre(models.Model):
+    name = models.CharField(max_length=225)
+    def __str__ (self):
+        return self.name
+
+class Playlist(models.Model):
+    name = models.CharField(max_length=225)
+    def __str__ (self):
+        return self.name
+
+class Song(models.Model):
+    title = models.CharField(max_length=225)
+    duration = models.models.DecimalField(max_digits = 3, decimal_places = 2, null=False)
+    artist_id = models.ForeignKey(Artist, on_delete=models.PROTECT)
+    album_id = models.ForeignKey(Album, on_delete=models.PROTECT)
+    genre_id = models.ForeignKey(Genre, on_delete=models.PROTECT)
+
+
+
+
